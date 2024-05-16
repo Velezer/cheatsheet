5b60f8279b71577352efef6210c12e1f011335c7
Arief Syaifuddin <>
2024-05-16 22:05:41 +0700

    alternative without postgis

--- database/postgre/distance.sql
+++ database/postgre/distance.sql
@@ -12,7 +12,7 @@ DECLARE
     random_lon DECIMAL;
     i INT := 1;
 BEGIN
-    WHILE i <= 1000000 LOOP
+    WHILE i <= 10000 LOOP
         -- Generate random latitude (-90 to 90 degrees)
         random_lat := -90.0 + (RANDOM() * 180.0);
         
@@ -32,6 +32,7 @@ $$ LANGUAGE plpgsql;
 
 -- apt-get install postgis;
 
+--drop extension postgis;
 CREATE EXTENSION postgis;
 
 -- Assuming the user provides the latitude and longitude values
@@ -64,11 +65,29 @@ WITH distances AS (
     where ST_DWithin(
                ST_SetSRID(ST_MakePoint(106.8376889, -6.213018), 4326),
                ST_SetSRID(ST_MakePoint(longitude, latitude), 4326),
-               1 -- 100km
+               2 -- 100km
            ) = true -- is in distance
+--    limit 1000
 )
-SELECT id, latitude, longitude, FLOOR(distance_in_meter * 100) / 100
+SELECT id, latitude, longitude, FLOOR(distance_in_meter * 100) / 100, 
+(SQRT(POW(69.1 * (latitude::float -  -6.213018::float), 2) + POW(69.1 * (106.8376889::float - longitude::float) * COS(latitude::float / 57.3), 2)) * 1609.344
+) distance_in_meter_math
 FROM distances
 WHERE true 
-ORDER BY distance_in_meter desc
+ORDER BY distance_in_meter asc
 ;
+
+-- alternative without postgis
+
+CREATE INDEX idx_latitude ON research (latitude);
+CREATE INDEX idx_longitude ON research (longitude);
+
+explain analyze
+SELECT id, latitude, longitude, 
+    (SQRT(POW(69.1 * (latitude::float - -6.213018), 2) + POW(69.1 * (106.8376889 - longitude::float) * COS(latitude::float / 57.3), 2)) * 1609.344) AS distance_in_meter
+FROM research
+WHERE latitude BETWEEN -6.213018 - 1 AND -6.213018 + 1
+  AND longitude BETWEEN 106.8376889 - 1 AND 106.8376889 + 1
+  AND (SQRT(POW(69.1  * (latitude::float - -6.213018), 2) + POW(69.1 * (106.8376889 - longitude::float) * COS(latitude::float / 57.3), 2)) * 1609.344) < 100000
+ORDER BY distance_in_meter;
+
